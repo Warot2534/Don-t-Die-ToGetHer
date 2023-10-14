43fe938578df65ebbc3ae4aad732187b329ce53c
Warot2534 <warmmy171131@gmail.com>
2023-10-14 14:07:52 +0000

    Auto commit at 2023-10-14 14:07:52

--- autocommit.sh
+++ autocommit.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+git remote set-url origin https://Warot2534:"$Access key"@github.com/Warot2534/Don-t-Die-ToGetHer.git
+git config --global user.email "warmmy171131@gmail.com"
+git config --global user.name "Warot2534"
 git add .
 git commit -m "Auto commit at $(date +'%Y-%m-%d %H:%M:%S')"
 git push origin main
