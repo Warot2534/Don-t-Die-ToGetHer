445ec42043438db4185af1db24775914d439cea8
Warot2534 <warmmy171131@gmail.com>
2023-10-14 14:27:13 +0000

    Auto commit at 2023-10-14 14:27:13

--- autocommit.sh
+++ autocommit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git remote set-url origin https://Warot2534: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@github.com/Warot2534/Don-t-Die-ToGetHer.git
+git remote set-url origin https://Warot2534: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@github.com/Warot2534/Don-t-Die-ToGetHer.git
 
 git config --global user.email "warmmy171131@gmail.com"
 git config --global user.name "Warot2534"
